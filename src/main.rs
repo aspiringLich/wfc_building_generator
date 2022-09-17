20e7aa7012bee8700bd0cb35bad2e54a1afcf7a2
aspiringLich <48413902+aspiringLich@users.noreply.github.com>
2022-09-16 21:40:23 -0400

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+use bevy::prelude::*;
+
+// TODO: Test the comments work
+// FIXME: make not broken
+// ? big question question question something idk
+// ! this is very important
+// * engineer gaming
+// coment
+
+fn main() {
+    println!("Hello, world!");
+}
